2d32926e9ff13016923ebc5582e1bcf48f07d254
Chris Scott <cscott530@gmail.com>
2015-05-04 23:30:06 -0400

    motion listeners.

--- TrigTutorial/TrigTutorial/GameScene.swift
+++ TrigTutorial/TrigTutorial/GameScene.swift
@@ -7,41 +7,54 @@
 //
 
 import SpriteKit
+import CoreMotion
 
 class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "Player")
+    let motionManager = CMMotionManager()
+    
+    var accelerometerX: UIAccelerationValue = 0
+    var accelerometerY: UIAccelerationValue = 0
+    
+    deinit {
+        self.stopMonitoringAcceleration()
+    }
     
     override func didMoveToView(view: SKView) {
         self.size = view.bounds.size
         
         self.backgroundColor = SKColor(red: 94.0 / 255.0, green: 63.0 / 255.0, blue: 107.0 / 255.0, alpha: 1.0)
         
-        player.position = CGPointMake(self.size.width / 2, self.size.height / 2)
+        self.player.position = CGPointMake(self.size.width / 2, self.size.height / 2)
         self.addChild(player)
+        
+        self.startMonitoringAcceleration()
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    //Moving the Player
+    func updatePlayerAccelerationFromMotionManager() {
+        if let acceleration = motionManager.accelerometerData?.acceleration {
+            let factor = 0.75
+            
+            accelerometerX = acceleration.x * factor + accelerometerX * (1 - FilterFactor)
+            accelerometerY = acceleration.y * factor + accelerometerY * (1 - FilterFactor)
+        }
+    }
+    
+    //Accelerometer
+    func startMonitoringAcceleration() {
+        if motionManager.accelerometerAvailable {
+            motionManager.startAccelerometerUpdates()
+        }
+    }
+    func stopMonitoringAcceleration() {
+        if motionManager.accelerometerAvailable && motionManager.accelerometerActive {
+            motionManager.stopAccelerometerUpdates()
+        }
     }
 }
